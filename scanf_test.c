1e91f735be137ee7cdef8800e9a309350f735336
0qqwq1 <plmi_123456@163.com>
2025-12-16 23:52:44 +0800

    同步了已完成的代码

---
+++ scanf_test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    if (scanf("%d, %d", &i, &j) == 1)
+    {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    return 0;
+}
