e120e7c852c928378c0c4a3e0e7429fad38287b3
0qqwq1 <plmi_123456@163.com>
2026-03-09 15:38:36 +0800

    P287-P290

---
+++ Chapter 11/DAY_7/nono.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    char side_a[] = "SIZE A";
+    char dont[] = { 'W', 'O', 'W', '!' }; //没有空字符
+    char size_b[] = "SIZE B";
+
+    puts(dont);
+    
+    return 0;
+}
