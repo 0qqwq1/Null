4a302505565d00dbb5d95d76262c7d61f5de50be
0qqwq1 <plmi_123456@163.com>
2026-03-11 14:42:35 +0800

    P290-P292

---
+++ Chapter 11/P290-P292_DAY8/put2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int put2(const char * string)
+{
+    int count = 0;
+    while (*string)
+    {
+        putchar(*string++);
+        count++;
+    }
+    putchar('\n');
+
+    return (count);
+}
+
+int main(void)
+{
+
+    return 0;
+}
